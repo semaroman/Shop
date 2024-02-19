270890c9dd7cac6d189f8aa1db822a5c99df4b4c
semaroman <106012219+semaroman@users.noreply.github.com>
2024-02-19 20:24:45 +0300

    Update OrderDateTime.java

--- src/OrderDateTime.java
+++ src/OrderDateTime.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class OrderDateTime {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class OrderDateTime {
+    private final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd:MM:yyyy HH:mm");
+    public String getCurrentDateTime() {
+        LocalDateTime date = LocalDateTime.now();
+        return dateFormat.format(date);
+    }
+    public long getMinutesDifference(String dateTimeString) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        LocalDateTime parsedDateTime = LocalDateTime.parse(dateTimeString, dateFormat);
+        return ChronoUnit.MINUTES.between(parsedDateTime, currentDateTime);
+    }
+
 }
