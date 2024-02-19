5f454bc74a87ac9f6ed1126e7c753eecf6653968
semaroman <106012219+semaroman@users.noreply.github.com>
2024-02-19 20:21:35 +0300

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Book {
+import java.util.List;
+
+public class Book extends AbstractProduct {
+
+    private String author;
+    private int pages;
+    private String genre;
+
+    public Book(String name, double price, double rating, String manufacturer, List<String> description) {
+        super(name, price, rating, manufacturer);
+        setDescription(description);
+    }
+
+    @Override
+    public void setDescription(List<String> description) {
+        this.author = description.get(0);
+        this.pages = Integer.parseInt(description.get(1));
+        this.genre = description.get(2);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Название: " + this.getName() + " | "
+                + "Автор: " + this.author + " | "
+                + "Жанр: " + this.genre + " | "
+                + "Количество страниц: " + this.pages + " | "
+                + "Издатель: " + this.getManufacturer() + " | "
+                + "Рейтинг: " + this.getRating() + " | "
+                + "Цена: " + this.getPrice();
+    }
 }
