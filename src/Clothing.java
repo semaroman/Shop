108112746fcb974a57573744de00ea56d5b8f36d
semaroman <106012219+semaroman@users.noreply.github.com>
2024-02-19 20:22:13 +0300

    Update Clothing.java

--- src/Clothing.java
+++ src/Clothing.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Clothing {
+import java.util.List;
+
+public class Clothing extends AbstractProduct {
+    private String material;
+    private String size;
+
+    public Clothing(String name, double price, double rating, String manufacturer, List<String> description) {
+        super(name, price, rating, manufacturer);
+        setDescription(description);
+    }
+
+    @Override
+    public void setDescription(List<String> description) {
+        this.material = description.get(0);
+        this.size = description.get(1);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Название: " + this.getName() + " | "
+                + "Материал: " + this.material + " | "
+                + "Размер: " + this.size + " | "
+                + "Производитель: " + this.getManufacturer() + " | "
+                + "Рейтинг: " + this.getRating() + " | "
+                + "Цена: " + this.getPrice();
+    }
 }
