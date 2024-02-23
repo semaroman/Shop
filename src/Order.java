96fabb343687dc03ac4df0b238d300fec0f744ef
semaroman <106012219+semaroman@users.noreply.github.com>
2024-02-23 10:15:46 +0300

    Update Order.java

--- src/Order.java
+++ src/Order.java
@@ -1,6 +1,5 @@
 /*
 Принцип разделения интерфейса (Interface Segregation Principle, ISP):
-
 Интерфейс Order определяет только операции, связанные с заказом, и не включает ненужные методы.
  */
 public interface Order {
