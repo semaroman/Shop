f13eeb4fa3d236dfce87a8eb93352f285ca37789
semaroman <106012219+semaroman@users.noreply.github.com>
2024-02-26 16:02:29 +0300

    Update Order.java

--- src/Order.java
+++ src/Order.java
@@ -1,8 +1,3 @@
-/*
-Принцип разделения интерфейса (Interface Segregation Principle, ISP):
-Интерфейс Order определяет только операции, связанные с заказом, и не включает ненужные методы.
- */
-
 public interface Order {
     void addProductToCustomerBucket(AbstractProduct product, int amount);
 }
